28f807f0f88650104bc92417f0e081bf9344204c
Serhii Borysenko <borisenko37@gmail.com>
2024-08-25 10:23:10 +0200

    add details about films, loader and error message

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -4,52 +4,73 @@ import { useParams } from "react-router-dom";
 import css from "./MovieDetailsPage.module.css";
 
 import { fetchMoviesDetails } from "../services/api";
+import Loader from "../../components/Loader/Loader";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
 const MovieDetailsPage = () => {
   const { moviesId } = useParams();
-  const [moviesDetails, setMoviesDetails] = useState([]);
+  const [moviesDetails, setMoviesDetails] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchMoviesByMoviesPage = async () => {
-      const data = await fetchMoviesDetails(moviesId);
-      console.log(data);
+      try {
+        setLoading(true);
+        const data = await fetchMoviesDetails(moviesId);
+        console.log(data);
 
-      setMoviesDetails(data);
+        setMoviesDetails(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchMoviesByMoviesPage();
   }, [moviesId]);
 
   return (
-    <div className={css.movieDetailsPage}>
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${moviesDetails.poster_path}`}
-        alt={moviesDetails.overview}
-        width={"250px"}
-      />
-      <div>
-        <h3 className={css.moviesDetailsTitle}>{moviesDetails.title}</h3>
-        <ul className={css.moviesDetailsList}>
-          <li className={css.moviesDetailsItem}>
-            <p className={css.moviesDetailsText}>
-              Data: {moviesDetails.release_date}
-            </p>
-          </li>
-          <li className={css.moviesDetailsItem}>
-            <p className={css.moviesDetailsText}>
-              User Score: {moviesDetails.vote_average}
-            </p>
-          </li>
-          <li className={css.moviesDetailsItem}>
-            <h3 className={css.moviesDetailsTitle}>Overview</h3>
-            <p className={css.moviesDetailsText}>{moviesDetails.overview}</p>
-          </li>
-          <li className={css.moviesDetailsItem}>
-            <h4>Genres</h4>
-            <p></p>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+      {loading && <Loader />}
+      {error !== null && <ErrorMessage errorMessage={error} />}
+      {moviesDetails !== null && (
+        <div className={css.movieDetailsPage}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${moviesDetails.poster_path}`}
+            alt={moviesDetails.overview}
+            width={"250px"}
+          />
+          <div>
+            <h3 className={css.moviesDetailsTitle}>{moviesDetails.title}</h3>
+            <ul className={css.moviesDetailsList}>
+              <li className={css.moviesDetailsItem}>
+                <p className={css.moviesDetailsText}>
+                  Data: {moviesDetails.release_date}
+                </p>
+              </li>
+              <li className={css.moviesDetailsItem}>
+                <p className={css.moviesDetailsText}>
+                  User Score: {moviesDetails.vote_average}
+                </p>
+              </li>
+              <li className={css.moviesDetailsItem}>
+                <h3 className={css.moviesDetailsTitle}>Overview</h3>
+                <p className={css.moviesDetailsText}>
+                  {moviesDetails.overview}
+                </p>
+              </li>
+              <li className={css.moviesDetailsItem}>
+                <h3 className={css.moviesDetailsTitle}>Genres</h3>
+                <p className={css.moviesDetailsText}>
+                  {moviesDetails?.genres?.map((genre) => genre.name).join(", ")}
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
