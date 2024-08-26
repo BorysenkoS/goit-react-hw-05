92a38c319792cd2f2192b102a80d7e499d20ee7b
Serhii Borysenko <borisenko37@gmail.com>
2024-08-26 19:33:02 +0200

    add message for component MovieCast

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -19,6 +19,7 @@ const MovieCast = () => {
       try {
         setLoading(true);
         const data = await fetchMoviesCredits(moviesId);
+
         setMoviesCredits(data);
       } catch (error) {
         setError(error.message);
@@ -29,23 +30,49 @@ const MovieCast = () => {
     fetchCredits();
   }, [moviesId]);
   return (
+    //  {moviesReviews?.results?.length !== 0 ? (
+    //         moviesReviews?.results?.map((result) => (
+    //           <div key={result.id}>
+    //             <h3>
+    //               <span>Author:</span> {result.author}
+    //             </h3>
+    //             <p>
+    //               <span>{result.content}</span>
+    //             </p>
+    //           </div>
+    //         ))
+    //       ) : (
+    //         <div className={css.reviewsMessage}>
+    //           <p>Sorry, but we don&apos;t have any reviews for this movie...</p>
+    //         </div>
+    //       )}
+
     <>
       {loading && <Loader />}
       {error !== null && <ErrorMessage errorMessage={error} />}
       <div className={css.movieCast}>
-        {moviesCredits?.cast?.map((cast) => (
-          <div key={cast.id} className={css.movieCastItem}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`}
-              width={200}
-              alt={cast.name}
-            />
-            <h3>{cast.name}</h3>
+        {moviesCredits?.cast?.length > 0 ? (
+          moviesCredits?.cast?.map((cast) => (
+            <div key={cast.id} className={css.movieCastItem}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`}
+                width={200}
+                alt={cast.name}
+              />
+              <h3>{cast.name}</h3>
+              <p>
+                Character: <span>{cast.character}</span>
+              </p>
+            </div>
+          ))
+        ) : (
+          <div className={css.castMessage}>
             <p>
-              Character: <span>{cast.character}</span>
+              Sorry, but we don&apos;t have any information about cast for this
+              movie...
             </p>
           </div>
-        ))}
+        )}
       </div>
     </>
   );
